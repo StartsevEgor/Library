6d08e4cba5abb4ac6b6e75070d680029d6fee9ed
StartsevEgor <148235400+StartsevEgor@users.noreply.github.com>
2023-10-29 22:37:08 +0300

    Add files via upload

--- library.py
+++ library.py
@@ -1,27 +1,57 @@
 import os
+import shutil
 
 from zipfile import ZipFile
 
 
 class Library:
-    def __init__(self, name=None):
+    def __init__(self):
+        self.name = None
+
+    def make(self):  # Создание пустой библиотеки
+        if os.path.isdir("Last library"):
+            shutil.rmtree("Last library")
+        os.mkdir("Last library")
+        open("Last library/database.sqlite", "w").close()
+        os.mkdir("Last library/Texts")
+        os.mkdir("Last library/Pictures")
+        self.name = None
+        self.save()
+
+    def open(self, name=None):  # Распаковка библиотеки в отдельную папку
         if name is None:
-            if not os.path.isdir("Last library"):
-                if os.path.isfile("last_libraries.txt"):
-                    self.last_libraries = open("last_libraries.txt",
-                                               "r").readlines()  # список последних открытых библиотек
-                    self.last_library = self.last_libraries[-1][:-1]
+            with open("last_libraries.txt", "r") as f:
+                self.name = f.readlines()[-1][:-1]
+                print(self.name)
+                ZipFile(self.name, "r").extractall(path="Last library")
+        else:
+            self.name = name.split("/")[-1]
+            ZipFile(self.name, "r").extractall(path="Last library")
+        self.save()
 
-                    os.mkdir("Last library")
-                    os.rename(self.last_library, self.last_library[:-3] + "zip")
-                    self.last_library = "Libraries/" + self.last_library[:-3] + "zip"
+    def save(self):
+        if self.name is not None:
+            name = self.name
+        else:
+            names = os.listdir("Libraries")
+            names = list(filter(lambda x: "Library_" in x, names))
+            if not names:
+                name = "Libraries/Library_1.lib"
+            else:
+                name = "Libraries/Library_" + str(max(list(map(lambda x: int(x[:-4].split("_")[1]), names)))) + ".lib"
 
-                    # Распаковка последней библиотеки в отдельную папку для более комфортной работы с ней
-                    ZipFile(self.last_library, "r").extractall(path="Last library")
-                else:
-                    self.last_libraries = open("last_libraries.txt", "w")
-                    self.last_libraries.write("Library.lib\n")
+        archive = ZipFile(name, mode="w")
+        archive.write("Last library/Pictures")
+        archive.write("Last library/Texts")
+        archive.write("Last library/database.sqlite")
+        archive.close()
 
-    def new_library(self, name):
-        name = ("Library_" + name) if name.isdigit() else name
-        database = open("Libraries/")
+        with open("last_libraries.txt", "r") as f:
+            file = f.readlines()
+            file = file if not file else file[:-1]
+        if name in file:
+            file.remove(name)
+        os.remove("last_libraries.txt")
+        with open("last_libraries.txt", "w") as f:
+            f.write(("\n".join(file) + "\n") if file else "")
+            f.write(f"{name}\n")
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
 import os
 
-from zipfile import ZipFile
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from main_window import Ui_MainWindow
@@ -12,21 +12,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main_window.ui', self)
+        self.initUI()
+
+    def new_library(self):
+        self.lib.save()
+        self.lib = Library()
+        self.lib.make()
+
+    def initUI(self):
         if not os.path.isdir("Last library"):
-            if os.path.isfile("last_libraries.txt"):
-                self.last_libraries = open("last_libraries.txt", "r").readlines()  # список последних открытых библиотек
-                self.last_library = self.last_libraries[-1][:-1]
-
-                os.mkdir("Last library")
-                os.rename(self.last_library, self.last_library[:-3] + "zip")
-                self.last_library = self.last_library[:-3] + "zip"
-
-                # Распаковка последней библиотеки в отдельную папку для более комфортной работы с ней
-                ZipFile(self.last_library, "r").extractall(path="Last library")
-            else:
-                self.last_libraries = open("last_libraries.txt", "w")
-                self.last_libraries.write("Library.lib\n")
-
+            self.lib = Library()
+            self.lib.make()
+        else:
+            self.lib = Library()
+            self.lib.open()
+        self.actionNew_library.triggered.connect(self.new_library)
 
     def open_book(self):
         self.book = BookText()
