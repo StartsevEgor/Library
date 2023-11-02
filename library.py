5f7c4d40eb7bfc6819cd93c82484cb5820253bbf
StartsevEgor <148235400+StartsevEgor@users.noreply.github.com>
2023-11-02 18:53:06 +0300

    Add files via upload

--- library.py
+++ library.py
@@ -1,57 +1,75 @@
 import os
 import shutil
+import sqlite3
 
 from zipfile import ZipFile
 
 
+def make_database():
+    con = sqlite3.connect("Last library/database.sqlite")
+    cur = con.cursor()
+    cur.executescript("""CREATE TABLE list_authors (ID INTEGER PRIMARY KEY AUTOINCREMENT, year TEXT);
+                CREATE TABLE list_years (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                year TEXT);
+                CREATE TABLE list_genres (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                genre TEXT);
+                CREATE TABLE books (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                author INTEGER REFERENCES list_authors (ID),
+                title TEXT,
+                year INTEGER REFERENCES list_years (ID),
+                genre INTEGER REFERENCES list_genres (ID),
+                file_with_text TEXT,
+                file_with_picture TEXT);
+                """)
+
+
 class Library:
-    def __init__(self):
-        self.name = None
+    def __init__(self, name=None, flag_new_file=True):
+        self.name = name
+        if (self.name is None) and flag_new_file:
+            names = os.listdir("Libraries")
+            names = list(filter(lambda x: "Library_" in x, names))
+            if not names:
+                self.name = "Libraries/Library_1.lib"
+            else:
+                self.name = "Libraries/Library_" + str(
+                    max(list(map(lambda x: int(x[:-4].split("_")[1]) + 1, names)))) + ".lib"
+
+    # def search(self, author, title, year, genre):
 
     def make(self):  # Создание пустой библиотеки
         if os.path.isdir("Last library"):
             shutil.rmtree("Last library")
         os.mkdir("Last library")
         open("Last library/database.sqlite", "w").close()
+        make_database()
         os.mkdir("Last library/Texts")
         os.mkdir("Last library/Pictures")
-        self.name = None
         self.save()
 
-    def open(self, name=None):  # Распаковка библиотеки в отдельную папку
-        if name is None:
+    def open(self):  # Распаковка библиотеки в отдельную папку
+        if self.name is None:
             with open("last_libraries.txt", "r") as f:
-                self.name = f.readlines()[-1][:-1]
-                print(self.name)
-                ZipFile(self.name, "r").extractall(path="Last library")
-        else:
-            self.name = name.split("/")[-1]
-            ZipFile(self.name, "r").extractall(path="Last library")
+                self.name = f.readlines()[-1].strip()
+        shutil.rmtree("Last library")
+        os.mkdir("Last library")
+        with ZipFile(self.name, "r") as archive:
+            archive.extractall(path="Last library")
         self.save()
 
     def save(self):
-        if self.name is not None:
-            name = self.name
-        else:
-            names = os.listdir("Libraries")
-            names = list(filter(lambda x: "Library_" in x, names))
-            if not names:
-                name = "Libraries/Library_1.lib"
-            else:
-                name = "Libraries/Library_" + str(max(list(map(lambda x: int(x[:-4].split("_")[1]), names)))) + ".lib"
-
-        archive = ZipFile(name, mode="w")
-        archive.write("Last library/Pictures")
-        archive.write("Last library/Texts")
-        archive.write("Last library/database.sqlite")
+        self.name += "\n" if "\n" not in self.name else ""
+        archive = ZipFile(self.name.strip(), mode="w")
+        archive.write("Last library/Pictures", "Pictures")
+        archive.write("Last library/Texts", "Texts")
+        archive.write("Last library/database.sqlite", "database.sqlite")
         archive.close()
-
         with open("last_libraries.txt", "r") as f:
             file = f.readlines()
-            file = file if not file else file[:-1]
-        if name in file:
-            file.remove(name)
+            file = file if file else []
+        if self.name in file:
+            file.remove(self.name)
         os.remove("last_libraries.txt")
+        file.append(self.name)
         with open("last_libraries.txt", "w") as f:
-            f.write(("\n".join(file) + "\n") if file else "")
-            f.write(f"{name}\n")
+            f.writelines(file)
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import sys
 import os
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QInputDialog
 from main_window import Ui_MainWindow
 from book_window import Ui_BookWindow
+from choice_book_window import Ui_ChoiceBookWindow
 from library import Library
 
 
@@ -14,29 +15,161 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         uic.loadUi('main_window.ui', self)
         self.initUI()
 
-    def new_library(self):
-        self.lib.save()
-        self.lib = Library()
-        self.lib.make()
-
     def initUI(self):
         if not os.path.isdir("Last library"):
             self.lib = Library()
             self.lib.make()
         else:
+            self.lib = Library(flag_new_file=False)
+            self.lib.open()
+        self.setWindowTitle(f"Библиотека - {self.lib.name}")
+        self.actionNew_library.triggered.connect(self.new_library)
+        self.actionOpen.triggered.connect(self.open_library)
+        self.actionOpenLastLibraries.triggered.connect(self.open_last_libraries)
+        self.actionSave.triggered.connect(self.lib.save)
+        self.actionSave_as.triggered.connect(self.save_as)
+        self.actionRename.triggered.connect(self.rename)
+        self.actionDelete.triggered.connect(self.delete)
+        self.actionAdd_book.triggered.connect(self.open_choice_book)
+        self.searchButton.clicked.connect(self.search)
+
+    def add_book(self):
+        file = QFileDialog.getOpenFileName(self, "Выбрать книгу", "", "Книга (*.txt)")[0]
+
+    def search(self):
+        search_by_author = self.authorEdit.setText() if self.authorEdit.setText() else "*"
+        search_by_title = self.titleEdit.setText() if self.titleEdit.setText() else "*"
+        search_by_year = self.yearEdit.setText() if self.yearEdit.setText() else "*"
+        search_by_genre = self.genreEdit.setText() if self.genreEdit.setText() else "*"
+
+    def delete(self):
+        os.remove(self.lib.name.strip())
+        with open("last_libraries.txt", "r") as f:
+            libraries = f.readlines()
+        os.remove("last_libraries.txt")
+        with open("last_libraries.txt", "w") as f:
+            f.writelines(libraries[:-1])
+        if len(libraries) > 1:
+            self.lib = Library(flag_new_file=False)
+            self.lib.open()
+        else:
             self.lib = Library()
             self.lib.open()
-        self.actionNew_library.triggered.connect(self.new_library)
+
+    def rename(self):
+        new_name, ok_pressed = QInputDialog.getText(self, "Переименовать", "Введите новое имя:")
+        if ok_pressed:
+            self.lib.name = "/".join(self.lib.name.split("/")[:-1]) + "/" + new_name + ".lib"
+            with open("last_libraries.txt", "r") as f:
+                last_libraries = f.readlines()
+                os.remove(last_libraries[-1].strip())
+            os.remove("last_libraries.txt")
+            with open("last_libraries.txt", "w") as f:
+                f.writelines(last_libraries[:-1])
+            self.lib.save()
+        self.setWindowTitle(f"Библиотека - {self.lib.name}")
+
+    def save_as(self):
+        with open("last_libraries.txt", "r") as libraries:
+            last_directory = "/".join(libraries.readlines()[-1].split("/")[:-1])
+        file = QFileDialog.getSaveFileName(self, "Сохранить", last_directory,
+                                           "Библиотека (*.lib);;Сжатая папка(*.zip)")[0]
+        os.remove(self.lib.name.strip())
+        self.lib.name = file
+        with open("last_libraries.txt", "r") as f:
+            last_libraries = f.readlines()
+        os.remove("last_libraries.txt")
+        with open("last_libraries.txt", "w") as f:
+            f.writelines(last_libraries[:-1])
+        self.lib.save()
+        self.setWindowTitle(f"Библиотека - {self.lib.name}")
+
+    def new_library(self):
+        self.lib.save()
+        self.lib = Library()
+        self.lib.make()
+        self.setWindowTitle(f"Библиотека - {self.lib.name}")
+
+    def open_library(self):
+        with open("last_libraries.txt", "r") as libraries:
+            last_directory = "/".join(libraries.readlines()[-1].split("/")[:-1])
+        file_name = QFileDialog.getOpenFileName(self, "Выбрать библиотеку", last_directory,
+                                                "Библиотека (*.lib);;Сжатая папка(*.zip)")[0]
+        if file_name:
+            self.lib.save()
+            self.lib = Library(file_name.strip(), flag_new_file=False)
+            self.lib.open()
+        self.setWindowTitle(f"Библиотека - {self.lib.name}")
+
+    def open_last_libraries(self):
+        with open("last_libraries.txt", "r") as libraries:
+            libraries = list(map(lambda x: x.strip(), libraries.readlines()))
+        library, ok_pressed = (
+            QInputDialog.getItem(self, "Выбрать библиотеку", "Выберите библиотеку", libraries, 1, False))
+        if ok_pressed:
+            self.lib.save()
+            self.lib = Library(library.strip(), flag_new_file=False)
+            self.lib.open()
+        self.setWindowTitle(f"Библиотека - {self.lib.name}")
 
     def open_book(self):
         self.book = BookText()
         self.book.show()
+
+    def open_choice_book(self):
+        self.choice_book = ChoiceBook()
+        self.choice_book.show()
+        self.choice_book.add_bookButton.clicked.connect(self.set_data)
+        self.choice_book.cancelButton.clicked.connect(self.choice_book.close)
+
+    def set_data(self):
+        self.flag, self.author, self.title, self.year, self.text, self.picture = self.choice_book.initUI()
+        print(self.author, self.title, self.year, self.text, self.picture)
+        if self.flag:
+            self.choice_book.close()
 
 
 class BookText(QMainWindow, Ui_BookWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('book_window.ui', self)
+
+
+class ChoiceBook(QMainWindow, Ui_ChoiceBookWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('choice_book_window.ui', self)
+        self.text = ""
+        self.picture = ""
+        self.textButton.clicked.connect(self.change_file_with_text)
+        self.pictureButton.clicked.connect(self.change_file_with_picture)
+
+    def change_file_with_text(self):
+        self.text = QFileDialog.getOpenFileName(self, "Выберите текстовый файл", "", "Текст (*.txt)")[0]
+        self.textEdit.setText(self.text)
+        self.update()
+
+    def change_file_with_picture(self):
+        types_pictures = "Изображение (*.png);;Изображение (*.jpg);;Изображение (*.jpeg);;Изображение (*.bmp)"
+        self.picture = QFileDialog.getOpenFileName(self, "Выберите файл с изображением", "", types_pictures)[0]
+        self.pictureEdit.setText(self.picture)
+        self.update()
+
+    def initUI(self):
+        author = self.authorEdit.text()
+        title = self.titleEdit.text()
+        year = self.yearEdit.text()
+        self.text = self.textEdit.text()
+        self.picture = self.pictureEdit.text()
+        if not all([author, title, year, self.text]):
+            flag = False
+            self.statusbar.showMessage("Необходимо заполнить все обязательные поля")
+        elif not os.path.isfile(self.text):
+            flag = False
+            self.statusbar.showMessage("Введите правильное название файла")
+        else:
+            flag = True
+        return flag, author, title, year, self.text, self.picture
 
 
 if __name__ == '__main__':
